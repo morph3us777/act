f50c49922812e11ff7667f5dff921b5a97677988
morph3us777 <morpheuscassian@gmail.com>
2025-06-04 22:06:26 -0500

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+// Discord Webhook Configuration
+define('DISCORD_WEBHOOK_URL', 'https://discord.com/api/webhooks/1379908278691037224/WTf1rp6emjYZ6S3LxYF6OO8cYXzvRO9s3vhJLi6tuMfDWTICd6rLRVnCUE-73XQQXpWX');
+
+// Otros ajustes de configuración pueden ir aquí
+?>
